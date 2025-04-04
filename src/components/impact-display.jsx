8b8eb905a7043de5a755508c623166bcb3b67b7b
Enachi Adrian <enachi.adrian52@gmail.com>
2025-04-05 01:29:27 +0200

    impact section responsive

--- src/components/impact-display.jsx
+++ src/components/impact-display.jsx
@@ -17,33 +17,33 @@ export default function ImpactDisplay() {
                     {/* Right: Text */}
                     <div className="impact-content">
                         <div className="impact-content-item">
-                            <div className="impact-content-item-icon">
+                            <div className="impact-content-item-header">
                                 <img src="./src/assets/icon1-small.svg" alt="Icon" />
+                                <h3>Biodiversity Loss</h3>
                             </div>
                             <div className="impact-content-item-text">
-                                <h3>Biodiversity Loss</h3>
                                 <p>
                                     Expanding palm oil plantations often replace rich tropical forests, leading to the loss of habitat for countless species. This habitat destruction threatens biodiversity and disrupts ecosystems.
                                 </p>
                             </div>
                         </div>
                         <div className="impact-content-item">
-                            <div className="impact-content-item-icon">
+                            <div className="impact-content-item-header">
                                 <img src="./src/assets/icon2-small.svg" alt="Icon" />
+                                <h3>Climate Impact</h3>
                             </div>
                             <div className="impact-content-item-text">
-                                <h3>Climate Impact</h3>
                                 <p>
                                     Deforestation for palm oil releases significant amounts of carbon dioxide into the atmosphere, contributing to climate change. The removal of trees diminishes the planet's capacity to absorb CO₂, exacerbating global warming.										
                                 </p>
                             </div>
                         </div>
                         <div className="impact-content-item">
-                            <div className="impact-content-item-icon">
+                            <div className="impact-content-item-header">
                                 <img src="./src/assets/icon3-small.svg" alt="Icon" />
+                                <h3>Soil and Water Pollution</h3>
                             </div>
                             <div className="impact-content-item-text">
-                                <h3>Soil and Water Pollution</h3>
                                 <p>
                                     Palm oil production generates substantial waste and can lead to the pollution of freshwater sources. The use of pesticides and fertilizers further contaminates soil and water, affecting both biodiversity and human communities.
                                 </p>
