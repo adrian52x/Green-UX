5884e1088572f903e5ac4a74d43b2a3f7e77008e
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2025-04-06 10:49:17 +0200

    margins on ph

--- src/components/hero-display.jsx
+++ src/components/hero-display.jsx
@@ -3,19 +3,74 @@ import "../styles/hero.css";
 export default function Hero() {
   return (
     <div className="container-hero">
-      <img src="/assets/bird-up.png" alt="bird" className="bird__up1" />
-      <img src="/assets/bird-up.png" alt="bird" className="bird__up2" />
-      <img src="/assets/bird-up.png" alt="bird" className="bird__up3" />
-      <img src="/assets/bird-up.png" alt="bird" className="bird__up4" />
+      <img
+        src="/assets/bird-up.png"
+        alt="bird"
+        aria-hidden="true"
+        className="bird__up1"
+      />
+      <img
+        src="/assets/bird-up.png"
+        alt="bird"
+        aria-hidden="true"
+        className="bird__up2"
+      />
+      <img
+        src="/assets/bird-up.png"
+        alt="bird"
+        aria-hidden="true"
+        className="bird__up3"
+      />
+      <img
+        src="/assets/bird-up.png"
+        alt="bird"
+        aria-hidden="true"
+        className="bird__up4"
+      />
 
-      <img src="/assets/bird-down.png" alt="bird" className="bird__down1" />
-      <img src="/assets/bird-down.png" alt="bird" className="bird__down2" />
-      <img src="/assets/bird-down.png" alt="bird" className="bird__down3" />
+      <img
+        src="/assets/bird-down.png"
+        alt="bird"
+        aria-hidden="true"
+        className="bird__down1"
+      />
+      <img
+        src="/assets/bird-down.png"
+        alt="bird"
+        aria-hidden="true"
+        className="bird__down2"
+      />
+      <img
+        src="/assets/bird-down.png"
+        alt="bird"
+        aria-hidden="true"
+        className="bird__down3"
+      />
 
-      <img src="/assets/stone.png" alt="bird" className="stone1" />
-      <img src="/assets/stone.png" alt="bird" className="stone2" />
-      <img src="/assets/stone.png" alt="bird" className="stone3" />
-      <img src="/assets/stone.png" alt="bird" className="stone4" />
+      <img
+        src="/assets/stone.png"
+        alt="tree stumps"
+        aria-hidden="true"
+        className="stone1"
+      />
+      <img
+        src="/assets/stone.png"
+        alt="tree stump"
+        aria-hidden="true"
+        className="stone2"
+      />
+      <img
+        src="/assets/stone.png"
+        alt="tree stump"
+        aria-hidden="true"
+        className="stone3"
+      />
+      <img
+        src="/assets/stone.png"
+        alt="tree stump"
+        aria-hidden="true"
+        className="stone4"
+      />
 
       <section className="hero-section">
         <div className="hero-left ">
@@ -24,7 +79,12 @@ export default function Hero() {
             alt="Back Tree"
             className="backtree"
           />
-          <img src="/assets/tree.svg" alt="Tree" className="tree" />
+          <img
+            src="/assets/tree.svg"
+            alt="Tree"
+            className="tree"
+            aria-hidden="true"
+          />
         </div>
 
         <div className="hero-right">
